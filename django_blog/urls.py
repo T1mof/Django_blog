2bd45471d77962663cf8a0b695ada3b5a37081ff
Tretyakov <timofey.tretyakov.96@mail.ru>
2024-05-17 03:28:16 +0300

    3

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django_blog import views
 
 urlpatterns = [
     path('', views.index),
+    path('articles/', include('django_blog.article.urls')),
     path('about/', views.about),
     path('admin/', admin.site.urls),
 ]
