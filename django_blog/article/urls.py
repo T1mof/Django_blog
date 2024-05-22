4e4e8ddd0d8cb18c2367eb07129d8a51232b3284
Tretyakov <timofey.tretyakov.96@mail.ru>
2024-05-22 21:46:41 +0300

    7

--- django_blog/article/urls.py
+++ django_blog/article/urls.py
@@ -4,4 +4,5 @@ from django_blog.article import views
 
 urlpatterns = [
     path('', views.Index.as_view()),
+    path('<str:tags>/<int:article_id>/', views.index, name='article'),
 ]
--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.urls import reverse
 
 class Index(View):
 
     def get(self, request, *args, **kwargs):
-        app_name = 'Аrticle'
-        return render(request, 'articles/article.html', context={'app_name': app_name})
+        return redirect(reverse("article", args={"tags": "python", 'article_id': 42}))
+
+def index(request, tags, article_id):
+    return render(request, 'articles/custom_article.html', context={'tags': tags, 'article_id': article_id})
