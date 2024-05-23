e5e2ba887be69dfd4cac2109c14a1ac8bcc8445f
Tretyakov <timofey.tretyakov.96@mail.ru>
2024-05-24 00:55:20 +0300

    11

--- django_blog/article/models.py
+++ django_blog/article/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Article(models.Model):
     name = models.CharField(max_length=200)
     body = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
