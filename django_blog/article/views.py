36751f9d2a4453f5beca3d7104c7c523fb6666b9
Tretyakov <timofey.tretyakov.96@mail.ru>
2024-05-26 02:31:34 +0300

    14

--- django_blog/article/views.py
+++ django_blog/article/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-
+from .forms import ArticleForm
 from django_blog.article.models import Article
+from django.contrib import messages
 
 class IndexView(View):
 
@@ -23,3 +24,17 @@ class ArticleCommentsView(View):
         comment = get_object_or_404(Comment, id=kwargs['id'], article_id=kwargs['article_id'])
 
         return render()
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Статья создана!')
+            return redirect('articles')
+        return render(request, 'articles/create.html', context={'form': form})
